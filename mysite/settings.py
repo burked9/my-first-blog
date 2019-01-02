2819a32957b3fb3f5cc16e219910907655c37e1e
burked9@gmail.com <daniel.burke@magellangroup.net>
2019-01-02 16:30:37 +0000

    Changed the CSS due to theoretical bug on PAW.

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+""" ## ALTERNATE DATABASE PSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'daniel',
+        'PASSWORD': '', # AN124-Ruslan
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+"""
 
 
 # Password validation
